29acfbfa0b98d7d17d6b31e6f5137938701e1bf8
amit <88amit77@gmail.com>
2020-06-09 18:55:59 +0530

    swagger setting added

--- orders/api/urls.py
+++ orders/api/urls.py
@@ -1,13 +1,19 @@
 from django.conf.urls import url
 from django.urls import path, include
-from rest_framework import routers
-from rest_framework_swagger.views import get_swagger_view
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 from rest_framework.routers import DefaultRouter
 from .orders import views
 from django.urls import path, include
 
 router = DefaultRouter()
-schema_view = get_swagger_view(title='Microservice API')
+schema_view = get_schema_view(openapi.Info(
+      title="<Instance Name> API",
+      default_version='v1',
+      description="Test description",
+   ), public=True, permission_classes=(permissions.AllowAny,))
+#schema_view = get_swagger_view(title='Microservice API')
 
 
 #each table Order APIs
@@ -38,7 +44,7 @@ router.register('list_manifest', views.ListManiFestViewSet, basename="list_manif
 
 urlpatterns = [
     path('', include(router.urls)),
-    path("docs/", schema_view),
+    path("orders_docs/", schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('order_view_search/', views.OrderViewSearchAPIView.as_view()),
     path('list_pod_list_search/', views.SearchListRMPODlistViewSet.as_view()),
     path('list_pod_Warehousewise_search/', views.SearchListRMPODlistWarehouseViewSet.as_view()),
